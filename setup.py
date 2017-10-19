ae4e2463ed6d6c6556d511a8e638863d52fb9502
Marcello Seri <marcello.seri@citrix.com>
2017-10-19 17:57:26 +0100

    Prepare to release 0.4
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xapitodict',
-      version='0.3',
+      version='0.4',
       packages=['xapitodict', 'xapitodict.cmd'],
       install_requires=["sexpdata", "xmltodict"],
       entry_points={
