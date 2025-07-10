c0799901f17727fd49aa21c7dad02bc565012b08
Pau Ruiz Safont <pau.safont@vates.tech>
2025-07-10 09:36:32 +0100

    Add setuptools to runtime dependencies
    This is needed in niewer tools since they might not provide setuptools
    to the venv the tool is installed in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='xapitodict',
       version='0.5.4',
       packages=['xapitodict', 'xapitodict.cmd'],
-      install_requires=["sexpdata", "xmltodict"],
+      install_requires=["sexpdata", "xmltodict", "setuptools"],
       entry_points={
           'console_scripts': [
               'xapi-to-json = xapitodict.cmd.xapitojson:main'
